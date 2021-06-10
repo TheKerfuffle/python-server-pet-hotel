2759aeb90cd291664f3b2db68d0fb25d2a15eb57
Ian Lambert <lambe347@umn.edu>
2021-06-10 13:16:40 -0500

    added component to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import AddPet from '../AddPet/AddPet'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddPet />
     </div>
   );
 }
