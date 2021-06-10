895043dd239f58d7dcdd99fb121979a0dee16242
Ian Lambert <lambe347@umn.edu>
2021-06-10 10:40:02 -0500

    basic input page set up, now for reducer/saga

--- python-api-server.py
+++ python-api-server.py
@@ -10,8 +10,7 @@ app.config["DEBUG"] = True
 conn = psycopg2.connect(
         host="localhost",
         port="5432",
-        user="charlie",
-        database="zoo_animals"
+        database="pet_hotel",
 )
 
 @app.route('/', methods=['GET'])
@@ -23,26 +22,24 @@ def home():
 def create_book():
     print('request.json is a dict!', request.json)
     print('if you\'re using multipart/form data, use request.form instead!', request.form)
-    title = request.json['title']
-    author = request.json['author']
     try:
         # Avoid getting arrays of arrays!
         cursor = conn.cursor(cursor_factory=RealDictCursor)
 
         print(title, author)
-        insertQuery = "INSERT INTO books (title, author) VALUES (%s, %s)"
+        insertQuery = "INSERT INTO owner (owner) VALUES (%s)"
         # if only only one param, still needs to be a tuple --> cursor.execute(insertQuery, (title,)) <-- comma matters!
         cursor.execute(insertQuery, (title, author))
         # really for sure commit the query
         conn.commit()
         count = cursor.rowcount
-        print(count, "Book inserted")
+        print(count, "Owner inserted")
         # respond nicely
         result = {'status': 'CREATED'}
         return jsonify(result), 201
     except (Exception, psycopg2.Error) as error:
         # there was a problem
-        print("Failed to insert book", error)
+        print("Failed to insert owner", error)
         # respond with error
         result = {'status': 'ERROR'}
         return jsonify(result), 500
